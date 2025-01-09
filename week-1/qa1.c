733befd19e841f848e5c2bb978f4ba2ec5f807dd
udita gowdety <uditagowdety@gmail.com>
2025-01-09 11:15:04 +0530

    solve additional Q1

--- week-1/qa1.c
+++ week-1/qa1.c
@@ -3,13 +3,29 @@
 
 int main(int argc, char *argv[]){
     int rank;
+    int nums[]={18,523,301,1234,2,14,108,150,1928};
+    int newarr[9];
 
     MPI_Init(&argc, &argv);
 
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+    int current=nums[rank];
+    int new=0;
+
+    while(current>0){
+        int digit=current%10;
+        new=new*10+digit;
+        current/=10;
+    }
+
+    nums[rank]=new;
+    newarr[rank]=new;
+
+    printf("process %d: %d\n",rank,nums[rank]);
     
     MPI_Finalize();
-
+    
     return 0;
 }
 
