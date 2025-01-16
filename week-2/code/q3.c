1c75c8398ceed4d44e66f399fa92f04a9f5da000
udita gowdety <uditagowdety@gmail.com>
2025-01-16 10:53:41 +0530

    minor syntax improvements in q3

--- week-2/code/q3.c
+++ week-2/code/q3.c
@@ -1,36 +1,41 @@
 #include "mpi.h"
 #include<stdio.h>
 
-int squared(int x){
-    return x*x;
+int squared(int x) {
+    return x * x;
 }
 
-int cubed(int x){
-    return x*x*x;
+int cubed(int x) {
+    return x * x * x;
 }
 
-int main(int argc, char* argv[]){
-    int rank,size;
-    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+int main(int argc, char* argv[]) {
+    int rank, size;
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    MPI_Init(&argc,&argv);
-    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-    MPI_Comm_size(MPI_COMM_WORLD,&size);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Status status;
 
-    if(rank==0){
-        for(int i=1;i<size;i++){
-            MPI_Send(&arr[i],1,MPI_INT,i,0,MPI_COMM_WORLD);
+    int buffer[100];  
+    MPI_Buffer_attach(buffer, sizeof(buffer));
+
+    if (rank == 0) {
+        for (int i = 1; i < size; i++) {
+            MPI_Bsend(&arr[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
     } else {
-        MPI_Recv(&arr[rank],1,MPI_INT,0,0,MPI_COMM_WORLD,&status);
+        MPI_Recv(&arr[rank], 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 
-        if(rank%2==0){
-            printf("process %d: received %d, squared result: %d\n",rank,arr[rank],squared(arr[rank]));
+        if (rank % 2 == 0) {
+            printf("process %d: received %d, squared result: %d\n", rank, arr[rank], squared(arr[rank]));
         } else {
-            printf("process %d: received %d, cubed result: %d\n",rank,arr[rank],cubed(arr[rank]));
+            printf("process %d: received %d, cubed result: %d\n", rank, arr[rank], cubed(arr[rank]));
         }
     }
+
+    MPI_Buffer_detach(&buffer, NULL);
 
     MPI_Finalize();
 
