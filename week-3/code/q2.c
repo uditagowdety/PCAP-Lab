2d83d13693f6160745f14c4d76f2489e087108c9
udita gowdety <uditagowdety@gmail.com>
2025-01-23 11:06:43 +0530

    shorten + annotate q2

--- week-3/code/q2.c
+++ week-3/code/q2.c
@@ -1,91 +1,50 @@
+// MPI program to calculate the average of averages:
+// 1. Root process (rank 0) reads a matrix of size m x n, where n is the number of processes.
+// 2. The matrix is divided row-wise and scattered to all processes.
+// 3. Each process calculates the average of its assigned rows.
+// 4. Root process gathers the local averages and computes the final average of all averages.
+
 #include "mpi.h"
 #include<stdio.h>
 #include<stdlib.h>
 
 int main(int argc, char* argv[]){
-    int rank,size,m,n;
-    int* numbers=NULL;
-    int* subarray=NULL;
-    float* averages=NULL;
+    int rank, size, m, *numbers = NULL, *subarray = NULL;
+    float *averages = NULL, local_avg, root_avg = 0;
 
-    MPI_Init(&argc,&argv);
-    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-    MPI_Comm_size(MPI_COMM_WORLD,&size);
-    MPI_Status status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    n=size;
-
-    if(rank==0){
+    if(rank == 0){
         printf("enter value of m: ");
-        scanf("%d",&m);
-
-        numbers=(int*)malloc(m*n*sizeof(int));
-
+        scanf("%d", &m);
+        numbers = (int*)malloc(m * size * sizeof(int));
         printf("enter mxn values:\n");
-        for(int i=0;i<m*n;i++){
-            scanf("%d",&numbers[i]);
-        }
-
-        averages=(float*)malloc(n*sizeof(float));
-    } 
-    MPI_Bcast(&m, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    subarray=(int*)malloc(m*sizeof(int));
-
-    // printf("Rank %d: m=%d, n=%d, sendcount=%d, recvcount=%d\n", rank, m, n, m, m);
-
-    MPI_Scatter(numbers,m,MPI_INT,subarray,m,MPI_INT,0,MPI_COMM_WORLD);
-
-    int local_sum=0;
-    for(int i=0;i<m;i++){
-        local_sum+=subarray[i];
+        for(int i = 0; i < m * size; i++) scanf("%d", &numbers[i]);
+        averages = (float*)malloc(size * sizeof(float));
     }
-    float local_avg=(float)local_sum/m;
 
-    printf("process %d: average = %f\n",rank,local_avg);
+    MPI_Bcast(&m, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    subarray = (int*)malloc(m * sizeof(int));
 
-    MPI_Gather(&local_avg,1,MPI_FLOAT,averages,1,MPI_FLOAT,0,MPI_COMM_WORLD);
+    MPI_Scatter(numbers, m, MPI_INT, subarray, m, MPI_INT, 0, MPI_COMM_WORLD);
 
-    if(rank==0){
-        float root_avg=0;
-        for(int i=0;i<n;i++){
-            root_avg+=averages[i];
-        }
-        root_avg/=n;
+    int local_sum = 0;
+    for(int i = 0; i < m; i++) local_sum += subarray[i];
+    local_avg = (float)local_sum / m;
 
-        printf("final average of all averages: %f\n",root_avg);
+    MPI_Gather(&local_avg, 1, MPI_FLOAT, averages, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
+    if(rank == 0){
+        for(int i = 0; i < size; i++) root_avg += averages[i];
+        root_avg /= size;
+        printf("final average of all averages: %f\n", root_avg);
         free(numbers);
         free(averages);
     }
 
     free(subarray);
-
     MPI_Finalize();
-
     return 0;
 }
-
-//read an integer value M, and N is the number of processes
-//read NxM elements into a 1D array in root process
-//root process sends M elements to each process
-//each process finds average of M elements that it received. sends the avg back to root
-//root collects all averages and finds average of them
-
-//notes:
-// Plan:
-
-//     Input Handling (Root Process):
-//         The root process (rank 0) will read the value of M and N.
-//         The root will read NxM elements into a 1D array.
-
-//     Distribute Data (MPI_Scatter):
-//         Use MPI_Scatter to send M elements to each of the processes, including the root process.
-
-//     Average Calculation (All Processes):
-//         Each process will calculate the average of the M elements it received.
-
-//     Send Back Averages to Root (MPI_Gather):
-//         Each process sends its calculated average back to the root using MPI_Gather.
-
-//     Root Computes Final Average:
-//         The root process will gather all averages and compute the final average of these averages.
